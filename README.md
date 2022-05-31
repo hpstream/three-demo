f776e62aeb22218a1f1b14aad6ec25181254bde9
huang.peng <huang.peng@immomo.com>
2022-05-31 17:01:15 +0800

    feat: 🎸 完成物体旋转

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## 项目启动
 > npm run dev
 
-## 常见概念
+## 1. 常见概念
 1. 场景 Sence
 2. 几何体(网格模型)Geometry。常见几何体：BoxGeometry,CubeGeometry。
 3. 材料 Mateiral, 创建材料：MeshBasicMaterial，MeshPongMaterial。
@@ -10,6 +10,13 @@
 6. 相机Camera。 
 7. 渲染器WebGLRenderer
 案例： 构建第一个学习3D场景【3d-one】
+
+### 物体旋转动画
+> setInterval,requestAnimationFrame
+案例： 旋转案例【rotate】
+
+
+
 
 
 
---
+++ pages/rotate/main.ts
@@ -0,0 +1,60 @@
+import * as THREE from "three";
+
+// 1. 创建一个场景
+let scene = new THREE.Scene();
+
+// 2.创建一个几何体，或者说创建网格模型
+let boxGeometry = new THREE.BoxGeometry(100, 100, 100);
+// 3. 创建材质
+// THREE.MeshBasicMaterial
+let material = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+
+// 4. 创建具体物体， 网格模型对象Mesh
+let boxMesh = new THREE.Mesh(boxGeometry, material);
+
+// 5. 将物体添加到场景中
+scene.add(boxMesh);
+
+//点光源
+var point = new THREE.PointLight(0xffffff);
+point.position.set(400, 200, 300); //点光源位置
+scene.add(point); //点光源添加到场景中
+//环境光
+var ambient = new THREE.AmbientLight(0x444444);
+scene.add(ambient);
+
+/**
+ * 相机设置
+ */
+var width = window.innerWidth; //窗口宽度
+var height = window.innerHeight; //窗口高度
+var k = width / height; //窗口宽高比
+var s = 200; //三维场景显示范围控制系数，系数越大，显示的范围越大
+//创建相机对象
+var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+camera.position.set(200, 300, 200); //设置相机位置
+camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+
+/**
+ * 创建渲染器对象
+ */
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height); //设置渲染区域尺寸
+renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
+document.body.appendChild(renderer.domElement); //body元素中插入canvas对象
+//执行渲染操作   指定场景、相机作为参数
+// renderer.render(scene, camera);
+
+// 渲染函数
+function render() {
+  renderer.render(scene, camera); //执行渲染操作
+  boxMesh.rotateY(0.01); //每次绕y轴旋转0.01弧度
+  requestAnimationFrame(render); //请求再次执行渲染函数render
+}
+// window.render = render;
+
+// setInterval(render, 1000 / 60);
+render();

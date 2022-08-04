1844cf0b4fed001aadf59b521bed8e37ba879f6f
huang.peng <huang.peng@immomo.com>
2022-08-04 18:10:44 +0800

    feat: 🎸 保存代码

---
+++ pages/02-basic-debug/03-animate/main.ts
@@ -0,0 +1,80 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+
+// 目标：了解threejs的基本内容
+
+// scene
+let scene = new THREE.Scene();
+
+/**
+ * 几何体
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 }); //红色
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// 物体移动
+// mesh.position.x = 2;
+// mesh.position.y = 2;
+// mesh.position.z = 2;
+// mesh.position.set(2, 2, 2)
+// 物体缩放
+// mesh.scale.x = 2;
+// mesh.scale.y = 2;
+// mesh.scale.z = 2;
+mesh.scale.set(2, 2, 2)
+
+// 物体旋转
+// mesh.rotateX(Math.PI / 4)
+// mesh.rotateY(Math.PI / 4)
+// mesh.rotateZ(Math.PI / 4)
+mesh.rotation.set(Math.PI / 4, Math.PI / 4, Math.PI / 4, 'XYZ');
+
+/**
+ * 相机设置
+ */
+var width = window.innerWidth; //窗口宽度
+var height = window.innerHeight; //窗口高度
+var k = width / height; //窗口宽高比
+
+var camera = new THREE.PerspectiveCamera(75, k, 0.1, 1000);
+camera.position.z = 10;
+camera.position.y = 10;
+camera.position.x = 10;
+
+let axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper)
+
+/**
+ * 渲染器
+ */
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+// camera.lookAt(0, 0, 0)
+renderer.render(scene, camera);
+document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
+
+let clock = new THREE.Clock()
+
+// 如果不重新绘制，物体会禁止就会不动
+function render(time: number) {
+
+  // 方式一
+  // let t = time / 1000 % 5;
+  // mesh.position.x = t*1;
+
+  // 方式二
+  let t = clock.getElapsedTime() % 5;
+  mesh.position.x = t * 1;
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(render)
+}
+window.requestAnimationFrame(render);
+
+
+
---
+++ pages/02-basic-debug/05.resize/main.ts
@@ -0,0 +1,66 @@
+import gsap from "gsap";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+
+// 目标：了解threejs的基本内容
+
+// scene
+let scene = new THREE.Scene();
+
+/**
+ * 几何体
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 }); //红色
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+
+/**
+ * 相机设置
+ */
+var width = window.innerWidth; //窗口宽度
+var height = window.innerHeight; //窗口高度
+var k = width / height; //窗口宽高比
+
+var camera = new THREE.PerspectiveCamera(75, k, 0.1, 1000);
+camera.position.z = 10;
+camera.position.y = 10;
+camera.position.x = 10;
+
+let axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper)
+
+/**
+ * 渲染器
+ */
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+// camera.lookAt(0, 0, 0)
+renderer.render(scene, camera);
+document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement)
+
+
+let animate = gsap.to(mesh.position, { x: 5, duration: 2, repeat: -1, yoyo: true, ease: "power1.inOut" })
+
+window.addEventListener('dblclick', () => {
+  if (animate.isActive()) {
+    animate.pause()
+  } else {
+    animate.play()
+  }
+})
+
+// 如果不重新绘制，物体会禁止就会不动
+function render(time: number) {
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(render)
+}
+window.requestAnimationFrame(render);
+
+
+

4f212fa6d38d19920c7e624cc496930e93ccbe11
huang.peng <huang.peng@immomo.com>
2022-08-11 16:45:26 +0800

    feat: 🎸 保存1

---
+++ pages/07-three_full_scroll/main.ts
@@ -0,0 +1,62 @@
+import "./assets/css/style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import * as dat from "dat.gui";
+
+// 目标：了解threejs的基本内容
+
+// scene
+let scene = new THREE.Scene();
+
+
+/**
+ * 相机设置
+ */
+var width = window.innerWidth; //窗口宽度
+var height = window.innerHeight; //窗口高度
+var k = width / height; //窗口宽高比
+
+var camera = new THREE.PerspectiveCamera(75, k, 0.1, 1000);
+camera.position.z = 15;
+camera.position.y = 15;
+camera.position.x = 15;
+
+let axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+/**
+ * 渲染器
+ */
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+camera.lookAt(0, 0, 0)
+renderer.render(scene, camera);
+document.body.appendChild(renderer.domElement);
+
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.listenToKeyEvents(document.body)
+
+// 如果不重新绘制，物体会禁止就会不动
+function render(time: number) {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+window.requestAnimationFrame(render);
+
+// 监听画面变化，更新渲染画面
+window.addEventListener("resize", () => {
+  //   console.log("画面变化了");
+  // 更新摄像头
+  camera.aspect = window.innerWidth / window.innerHeight;
+  //   更新摄像机的投影矩阵
+  camera.updateProjectionMatrix();
+
+  //   更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //   设置渲染器的像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
+

5c9bd6e8bc12ae00dde172530dc6e93f789dfd89
huang.peng <huang.peng@immomo.com>
2022-08-21 15:59:27 +0800

    feat: 🎸 保存1

--- pages/10.three_shader/04.basic-图形/shader/deep/fragment.glsl
+++ pages/10.three_shader/04.basic-图形/shader/deep/fragment.glsl
@@ -4,9 +4,36 @@ precision lowp float;
 varying vec2 vUv;
 // 获取时间
 uniform float uTime;
+
+#define PI 3.1415926535897932384626433832795
 // 随机函数
 float random (vec2 st) {
     return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);
+}
+// 旋转函数
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+// 噪声函数
+float noise (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
 }
 void main() {
     // 1通过顶点对应的uv，决定每一个像素在uv图像的位置，通过这个位置x,y决定颜色
@@ -109,8 +136,134 @@ void main() {
     // gl_FragColor = vec4(strength, strength, strength, 1);
 
     // 24随机效果
-    float strength = random(vUv);
+    // float strength = random(vUv);
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 24随机+格子效果
+    // float strength = ceil(vUv.x*10.0)/10.0*ceil(vUv.y*10.0)/10.0;
+    // strength = random(vec2(strength,strength));
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 25 依据length返回向量长度
+    // float strength = length(vUv);
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 26 根据distance技术2个向量的距离
+    // float strength =1.0 - distance(vUv,vec2(0.5,0.5));
+    // gl_FragColor = vec4(strength,strength,strength,1);
+
+    // 27根据相除，实现星星
+    // float strength =0.15 / distance(vUv,vec2(0.5,0.5)) - 1.0;
+    // gl_FragColor =vec4(strength,strength,strength,strength);
+
+    // 28设置vUv水平或者竖直变量
+    // float strength =0.15 / distance(vec2(vUv.x,(vUv.y-0.5)*5.0),vec2(0.5,0.5)) - 1.0;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+     // 29十字交叉的星星
+    // float  strength = 0.15 / distance(vec2(vUv.x,(vUv.y-0.5)*5.0+0.5),vec2(0.5,0.5)) - 1.0;
+    // strength += 0.15 / distance(vec2(vUv.y,(vUv.x-0.5)*5.0+0.5),vec2(0.5,0.5)) - 1.0;
+    // gl_FragColor =vec4(strength,strength,strength,strength);
+
+      // 29旋转飞镖，旋转uv
+    // vec2 rotateUv = rotate(vUv,3.14*0.25,vec2(0.5));
+    // vec2 rotateUv = rotate(vUv,-uTime*5.0,vec2(0.5));
+    // float  strength = 0.15 / distance(vec2(rotateUv.x,(rotateUv.y-0.5)*5.0+0.5),vec2(0.5,0.5)) - 1.0;
+    // strength += 0.15 / distance(vec2(rotateUv.y,(rotateUv.x-0.5)*5.0+0.5),vec2(0.5,0.5)) - 1.0;
+    // gl_FragColor =vec4(strength,strength,strength,strength);
+
+    // 31绘制圆
+    // float strength = 1.0 - step(0.5,distance(vUv,vec2(0.5))+0.25) ;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 32圆环
+    // float strength = 1.0 - step(0.5,distance(vUv,vec2(0.5,0.5))) ;
+    // strength *=  step(0.4,distance(vUv,vec2(0.5,0.5)));
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 34渐变环
+    // float strength = abs(distance(vUv,vec2(0.5))-0.2) ;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 35打靶
+    // float strength = step(0.1,abs(distance(vUv,vec2(0.5))-0.25));
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 36圆环
+    // float strength = 1.0 - step(0.1,abs(distance(vUv,vec2(0.5))-0.25)) ;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 37波浪环
+    // vec2 waveUv = vec2(
+    //     vUv.x,
+    //     vUv.y+sin(vUv.x*30.0)*0.1
+    // );
+    // float strength = 1.0 - step(0.01,abs(distance(waveUv,vec2(0.5))-0.25))   ;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 38
+    // vec2 waveUv = vec2(
+    //     vUv.x+sin(vUv.y*30.0)*0.1,
+    //     vUv.y+sin(vUv.x*30.0)*0.1
+    // );
+    // float strength = 1.0 - step(0.01,abs(distance(waveUv,vec2(0.5))-0.25))   ;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 39
+    // vec2 waveUv = vec2(
+    //     vUv.x+sin(vUv.y*100.0)*0.1,
+    //     vUv.y+sin(vUv.x*100.0)*0.1
+    // );
+    // float strength = 1.0 - step(0.01,abs(distance(waveUv,vec2(0.5))-0.25))   ;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 40 根据角度显示视图
+    // float angle = atan(vUv.x,vUv.y);
+    // float strength = angle;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 41 根据角度实现螺旋渐变
+    // float angle = atan(vUv.x-0.5,vUv.y-0.5);
+    // float strength = (angle+3.14)/6.28;
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 42 实现雷达扫射
+    // float alpha =  1.0 - step(0.5,distance(vUv,vec2(0.5)));
+    // float angle = atan(vUv.x-0.5,vUv.y-0.5);
+    // float strength = (angle+3.14)/6.28;
+    // gl_FragColor =vec4(strength,strength,strength,alpha);
+
+    // 43 通过时间实现动态选择
+    // vec2 rotateUv = rotate(vUv,3.14*0.25,vec2(0.5));
+    // vec2 rotateUv = rotate(vUv,-uTime*5.0,vec2(0.5));
+    // float alpha =  1.0 - step(0.5,distance(vUv,vec2(0.5)));
+    // float angle = atan(rotateUv.x-0.5,rotateUv.y-0.5);
+    // float strength = (angle+3.14)/6.28;
+    // gl_FragColor =vec4(strength,strength,strength,alpha);
+
+    // 44 万花筒 [-PI/2,PI/2] 
+    // float angle = atan(vUv.x-0.5,vUv.y-0.5)/PI;
+    // float strength = mod(angle*10.0,1.0);
+
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // 45 光芒四射
+    // float angle = atan(vUv.x-0.5,vUv.y-0.5)/(2.0*PI);
+    // float strength = sin(angle*100.0);
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+
+    // 46 使用噪声实现烟雾、波纹效果
+    // float strength = noise(vUv);
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+    // float strength = noise(vUv * 10.0);
+    // gl_FragColor =vec4(strength,strength,strength,1);
+
+
+    float strength = step(0.5,noise(vUv * 100.0)) ;
     gl_FragColor =vec4(strength,strength,strength,1);
+
 
 
 

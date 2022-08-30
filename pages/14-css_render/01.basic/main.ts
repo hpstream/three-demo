754958f180b93d1fc0bdc036aa8f81e81f3f99cb
huang.peng <huang.peng@immomo.com>
2022-08-30 10:51:35 +0800

    feat: 🎸 完成联系

--- pages/14-css_render/01.basic/main.ts
+++ pages/14-css_render/01.basic/main.ts
@@ -1,175 +1,164 @@
 import * as THREE from "three";
-
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import gsap from "gsap";
-import * as dat from "dat.gui";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import {
+   CSS2DRenderer,
+   CSS2DObject,
+} from "three/examples/jsm/renderers/CSS2DRenderer.js";
 
-// 导入后期效果合成器
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+let camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer, labelRenderer;
 
-// three框架本身自带效果
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass';
-import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass'
-import { SSAARenderPass } from 'three/examples/jsm/postprocessing/SSAARenderPass'
-import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
-
-
-// 目标：后期处理
-
-//创建gui对象
-const gui = new dat.GUI();
-
-// console.log(THREE);
-// 初始化场景
-const scene = new THREE.Scene();
-
-// 创建透视相机
-const camera = new THREE.PerspectiveCamera(
-   75,
-   window.innerHeight / window.innerHeight,
-   1,
-   50
-);
-// 设置相机位置
-// object3d具有position，属性是1个3维的向量
-camera.position.set(0, 0, 3);
-// 更新摄像头
-camera.aspect = window.innerWidth / window.innerHeight;
-//   更新摄像机的投影矩阵
-camera.updateProjectionMatrix();
-scene.add(camera);
-
-// 加入辅助轴，帮助我们查看3维坐标轴
-// const axesHelper = new THREE.AxesHelper(5);
-// scene.add(axesHelper);
-
-// 加载纹理
-
-// 创建纹理加载器对象
+const clock = new THREE.Clock();
 const textureLoader = new THREE.TextureLoader();
 
-
-// 添加环境纹理
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const envMapTexture = cubeTextureLoader.load([
-   "textures/environmentMaps/0/px.jpg",
-   "textures/environmentMaps/0/nx.jpg",
-   "textures/environmentMaps/0/py.jpg",
-   "textures/environmentMaps/0/ny.jpg",
-   "textures/environmentMaps/0/pz.jpg",
-   "textures/environmentMaps/0/nz.jpg",
-]);
-scene.background = envMapTexture;
-scene.environment = envMapTexture;
+let moon: THREE.Mesh, chinaLabel: CSS2DObject, earth: THREE.Mesh;
+const raycaster = new THREE.Raycaster();
+init();
+animate();
 
 
+// 创建射线
 
-const directionLight = new THREE.DirectionalLight('#ffffff', 1);
-directionLight.castShadow = true;
-directionLight.position.set(0, 0, 200)
-scene.add(directionLight)
+function init() {
+   const EARTH_RADIUS = 1;
+   const MOON_RADIUS = 0.27;
 
+   camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      200
+   );
+   camera.position.set(0, 5, -10);
 
+   scene = new THREE.Scene();
 
-// 模型加载
-const gltfLoader = new GLTFLoader();
-gltfLoader.load('./models/DamagedHelmet/glTF/DamagedHelmet.gltf', (gltf) => {
-   console.log(gltf.scene)
-   scene.add(gltf.scene)
-   const mesh = gltf.scene.children[0];
-   // console.log(gltf.scene)
-   scene.add(mesh)
-})
-
-
-// 初始化渲染器
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-// 设置渲染尺寸大小
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.toneMapping = THREE.ReinhardToneMapping;
-renderer.shadowMap.enabled = true;
+   const dirLight = new THREE.DirectionalLight(0xffffff);
+   dirLight.position.set(0, 0, 1);
+   scene.add(dirLight);
+   const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
+   scene.add(light);
 
 
-// 合成效果
-const effectComposer = new EffectComposer(renderer);
-effectComposer.setSize(window.innerWidth, window.innerHeight);
+   const earthGeometry = new THREE.SphereGeometry(EARTH_RADIUS, 16, 16);
+   const earthMaterial = new THREE.MeshPhongMaterial({
+      specular: 0x333333,
+      shininess: 5,
+      map: textureLoader.load("./textures/planets/earth_atmos_2048.jpg"),
+      specularMap: textureLoader.load("textures/planets/earth_specular_2048.jpg"),
+      normalMap: textureLoader.load("./textures/planets/earth_normal_2048.jpg"),
+      normalScale: new THREE.Vector2(0.85, 0.85),
+   });
 
-// 添加渲染通道
-const renderPass = new RenderPass(scene, camera);
-effectComposer.addPass(renderPass)
+   earth = new THREE.Mesh(earthGeometry, earthMaterial);
+   // earth.rotation.y = Math.PI;
+   scene.add(earth);
 
-// 点效果
-const dotScreenPass = new DotScreenPass();
-dotScreenPass.enabled = false;
-effectComposer.addPass(dotScreenPass)
-
-
-// // 抗锯齿
-// const smaaPass = new SMAAPass(window.innerWidth * renderer.getPixelRatio(), window.innerHeight * renderer.getPixelRatio());
-// effectComposer.addPass(smaaPass)
-
-// 发光效果
-const unrealBloomPass = new UnrealBloomPass(new THREE.Vector2(256, 256), 1, 0, 0);
-effectComposer.addPass(unrealBloomPass)
-// unrealBloomPass.bloomThreshold = 1;
-unrealBloomPass.strength = 1;
-unrealBloomPass.radius = 0;
-unrealBloomPass.threshold = 1;
-
-const glitchPass = new GlitchPass();
-effectComposer.addPass(glitchPass)
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1;
+   const moonGeometry = new THREE.SphereGeometry(MOON_RADIUS, 16, 16);
+   const moonMaterial = new THREE.MeshPhongMaterial({
+      shininess: 5,
+      map: textureLoader.load("textures/planets/moon_1024.jpg"),
+   });
+   moon = new THREE.Mesh(moonGeometry, moonMaterial);
+   scene.add(moon);
 
 
-gui.add(renderer, 'toneMappingExposure').min(0).max(2).step(0.01);
-gui.add(unrealBloomPass, 'strength').min(0).max(2).step(0.01);
-gui.add(unrealBloomPass, 'radius').min(0).max(2).step(0.01);
-gui.add(unrealBloomPass, 'threshold').min(0).max(2).step(0.01);
+   // 添加提示标签
+   const earthDiv = document.createElement('div');
+   earthDiv.className = "label";
+   earthDiv.innerHTML = "地球";
+   const earthLabel = new CSS2DObject(earthDiv);
+   earthLabel.position.set(0, 1, 0);
+   earth.add(earthLabel)
+
+   // 中国
+   const chinaDiv = document.createElement('div');
+   chinaDiv.className = "label1";
+   chinaDiv.innerHTML = "中国";
+   chinaLabel = new CSS2DObject(chinaDiv);
+   chinaLabel.position.set(-0.3, 0.5, -0.9);
+   earth.add(chinaLabel);
+   console.log(chinaLabel)
+
+   const moonDiv = document.createElement('div');
+   moonDiv.className = "label";
+   moonDiv.innerHTML = "月球";
+   const moonLabel = new CSS2DObject(moonDiv);
+   moonLabel.position.set(0, 0.3, 0);
+   moon.add(moonLabel);
 
 
+   // 实例化css2d的渲染器
+   labelRenderer = new CSS2DRenderer();
+   labelRenderer.setSize(window.innerWidth, window.innerHeight);
+   document.body.appendChild(labelRenderer.domElement)
+   labelRenderer.domElement.style.position = 'fixed';
+   labelRenderer.domElement.style.top = '0px';
+   labelRenderer.domElement.style.left = '0px';
+   labelRenderer.domElement.style.zIndex = '10';
 
-// 监听屏幕大小改变的变化，设置渲染的尺寸
-window.addEventListener("resize", () => {
-   //   console.log("resize");
-   // 更新摄像头
-   camera.aspect = window.innerWidth / window.innerHeight;
-   //   更新摄像机的投影矩阵
-   camera.updateProjectionMatrix();
-
-   //   更新渲染器
-   renderer.setSize(window.innerWidth, window.innerHeight);
-   //   设置渲染器的像素比例
+   renderer = new THREE.WebGLRenderer();
    renderer.setPixelRatio(window.devicePixelRatio);
-
-   effectComposer.setSize(window.innerWidth, window.innerHeight);
-   effectComposer.setPixelRatio(window.devicePixelRatio);
-});
+   renderer.setSize(window.innerWidth, window.innerHeight);
+   document.body.appendChild(renderer.domElement);
 
 
-// 将渲染器添加到body
-document.body.appendChild(renderer.domElement);
-
-// 初始化控制器
-const controls = new OrbitControls(camera, renderer.domElement);
-// 设置控制器阻尼
-controls.enableDamping = true;
-// 设置自动旋转
-// controls.autoRotate = true;
-
-const clock = new THREE.Clock()
-function animate() {
-   controls.update();
-   const time = clock.getElapsedTime();
-   requestAnimationFrame(animate);
-   // 使用渲染器渲染相机看这个场景的内容渲染出来
-   // renderer.render(scene, camera);
-   effectComposer.render()
+   const controls = new OrbitControls(camera, labelRenderer.domElement);
+   controls.minDistance = 5;
+   controls.maxDistance = 100;
+   window.addEventListener("resize", onWindowResize);
 }
 
-animate();
+function onWindowResize() {
+   camera.aspect = window.innerWidth / window.innerHeight;
+
+   camera.updateProjectionMatrix();
+
+   renderer.setSize(window.innerWidth, window.innerHeight);
+   labelRenderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+function animate() {
+   requestAnimationFrame(animate);
+
+   const elapsed = clock.getElapsedTime();
+
+
+   moon.position.set(Math.sin(elapsed) * 5, 0, Math.cos(elapsed) * 5);
+   moon.rotation.set(0, elapsed * 0.2, 0);
+   // earth.rotation.set(0, elapsed * 0.2, 0);
+
+   const chinaPosition = chinaLabel.position.clone();
+   // 计算出标签跟摄像机的距离
+   const labelDistance = chinaPosition.distanceTo(camera.position);
+   // 检测射线的碰撞
+   // chinaLabel.position
+   // 向量(坐标)从世界空间投影到相机的标准化设备坐标 (NDC) 空间。
+   chinaPosition.project(camera);
+   raycaster.setFromCamera(chinaPosition, camera);
+
+   const intersects = raycaster.intersectObjects(scene.children, true)
+   // console.log(intersects)
+
+   // 如果没有碰撞到任何物体，那么让标签显示
+   if (intersects.length == 0) {
+      chinaLabel.element.classList.add('visible');
+
+   } else {
+      // if(labelDistance)
+      const minDistance = intersects[0].distance;
+      console.log(minDistance, labelDistance)
+      if (minDistance < labelDistance) {
+         chinaLabel.element.classList.remove('visible');
+      } else {
+         chinaLabel.element.classList.add('visible');
+      }
+
+   }
+
+
+
+   // 标签渲染器渲染
+   labelRenderer.render(scene, camera);
+   renderer.render(scene, camera);
+}
